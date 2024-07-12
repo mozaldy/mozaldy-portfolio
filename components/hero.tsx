bcc9e5610463e490b8126573713602aa341efaeb
mozaldy <moxaldy@gmail.com>
2024-07-12 21:23:00 +0700

    Hero section done

---
+++ components/hero.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Spotlight } from "./ui/spotlights";
+import { TextGenerateEffect } from "./ui/text-generate-effects";
+import { MagicButton } from "./ui/magic-button";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="yellow"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] absolute flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            personal portfolio website
+          </p>
+          <TextGenerateEffect
+            words="Enthusiastic IT student ready to create"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hello! I&apos;m Rizaldy, a full-stack web developer and cyber
+            security enthusiast from Indonesia
+          </p>
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
