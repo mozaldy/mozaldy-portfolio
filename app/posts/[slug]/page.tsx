3943529ef17011a9ee8760056fcb2e6f6e707d48
mozaldy <moxaldy@gmail.com>
2024-07-15 03:02:52 +0700

    Use MDX properly to render markdown content

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,9 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
-import { MDXRemote } from "next-mdx-remote";
-import { serialize } from "next-mdx-remote/serialize";
-import Markdown from "markdown-to-jsx";
+import { MDXRemote } from "next-mdx-remote/rsc";
 import { FaHome } from "react-icons/fa";
 
 const postsDir = path.join(process.cwd(), "posts");
@@ -23,16 +21,17 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const fullPath = path.join(postsDir, `${slug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
-  const mdxSource = await serialize(content); // Serialize the MDX content
 
   return (
     <>
       <Link href="/">
-        <button className="px-5 py-2 rounded-full bg-slate-200 text-slate-950">
+        <button className="px-5 py-2 font-semibold rounded-full bg-slate-200 text-slate-950">
           <FaHome />
         </button>
       </Link>
-      <Markdown>{content}</Markdown>
+      <div className="my-10 px-2 prose prose-headings:text-slate-200 prose-p:text-slate-100">
+        <MDXRemote source={content} />
+      </div>
     </>
   );
 }
